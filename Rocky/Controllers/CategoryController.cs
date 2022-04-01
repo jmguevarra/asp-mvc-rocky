d28e4b073aeb09860bb65ce9a5be8d2405f397de
jmguevarra <jaimeguevarra22@gmail.com>
2022-04-01 17:58:58 +1100

    Data display from local db

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategory = _db.Category;
+            return View(objCategory);
+        }
+
+
+        public IActionResult CreateCategory(){
+            return View();
+        }
+    }
+}
